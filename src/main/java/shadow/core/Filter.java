e04aa78c4de84ecb9d4a4f9f484b7c9a7f7ad3e0
Alejandro Vera <xafilox@gmail.com>
2016-03-28 14:05:18 +0200

    Filter class to create WHERE query filters

--- src/main/java/shadow/core/Filter.java
+++ src/main/java/shadow/core/Filter.java
@@ -1,5 +1,70 @@
 package shadow.core;
 
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Filter {
+	
+	private String txt;
+	private List<Object> values;
+	
+	
+	public Filter(String element, String operator, Object value) {
+		this.txt = element.trim() + " " + operator.trim() + " ?";
+		this.values = new LinkedList<Object>();
+		this.values.add(value);
+	}
+
+	private Filter(String txt, List<Object> values) {
+		this.txt = txt;
+		this.values = values;
+	}
+
+	public String getTxt() {
+		return txt;
+	}
+
+	public List<Object> getValues() {
+		return Collections.unmodifiableList(values);
+	}
+
+	public static Filter and(Filter... filters) {
+		return joinFilters(" AND ", filters);
+	}
+	
+	public static Filter or(Filter... filters) {
+		return joinFilters(" OR ", filters);
+	}
+	
+	private static Filter joinFilters(String separator, Filter... filters) {
+		
+		String text = "";
+		List<Object> values = new LinkedList<Object>();
+		boolean first = true;
+		
+		for(Filter filter: filters) {
+			
+			// Create a list with the values for each paramenter in order
+			values.addAll(filter.values);
+			
+			// Generate text for a prepared statement
+			if(!first) {
+				text += separator;
+			} else {
+				first = false;
+			}
+			
+			if(filter.values.size() > 1) {
+				text += "(" + filter.txt + ")";
+			} else {
+				text += filter.txt;
+			}
+			
+		}
+		
+		return new Filter(text, values);
+		
+	}
 	
 }
---
+++ src/test/java/shadow/test/FilterTest.java
@@ -0,0 +1,86 @@
+package shadow.test;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import shadow.core.Filter;
+
+public class FilterTest {
+
+	@Test
+	public void testFilterConstructor() {
+		Filter fil = new Filter("name", "=", "Carl");
+		assertNotNull(fil);
+	}
+
+	@Test
+	public void testGetTxt() {
+		Filter fil = new Filter("name", "=", "Carl");
+		assertEquals("name = ?", fil.getTxt());
+	}
+
+	@Test
+	public void testGetValues() {
+		Filter fil = new Filter("name", "=", "Carl");
+		List<Object> vals = fil.getValues();
+		assertEquals(1, vals.size());
+		assertEquals("Carl", vals.get(0));
+	}
+
+	@Test
+	public void testAnd() {
+		Filter fil = Filter.and(
+				new Filter("name", "=", "Carl"),
+				new Filter("surname", "=", "Shadow"),
+				new Filter("age", ">=", 18)
+				);
+		
+		assertEquals("name = ? AND surname = ? AND age >= ?", fil.getTxt());
+		
+		List<Object> vals = fil.getValues();
+		assertEquals(3, vals.size());
+		assertEquals("Carl", vals.get(0));
+		assertEquals("Shadow", vals.get(1));
+		assertEquals(18, vals.get(2));
+	}
+
+	@Test
+	public void testOr() {
+		Filter fil = Filter.or(
+				new Filter("name", "=", "Carl"),
+				new Filter("surname", "=", "Shadow"),
+				new Filter("age", ">=", 18)
+				);
+		
+		assertEquals("name = ? OR surname = ? OR age >= ?", fil.getTxt());
+		
+		List<Object> vals = fil.getValues();
+		assertEquals(3, vals.size());
+		assertEquals("Carl", vals.get(0));
+		assertEquals("Shadow", vals.get(1));
+		assertEquals(18, vals.get(2));
+	}
+	
+	@Test
+	public void testOrWithAnd() {
+		Filter fil = Filter.or(
+				new Filter("name", "=", "Carl"),
+				Filter.and(
+					new Filter("surname", "=", "Shadow"),
+					new Filter("age", ">=", 18)
+					)
+				);
+		
+		assertEquals("name = ? OR (surname = ? AND age >= ?)", fil.getTxt());
+		
+		List<Object> vals = fil.getValues();
+		assertEquals(3, vals.size());
+		assertEquals("Carl", vals.get(0));
+		assertEquals("Shadow", vals.get(1));
+		assertEquals(18, vals.get(2));
+	}
+
+}
